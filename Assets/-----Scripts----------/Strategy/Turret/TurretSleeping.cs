d905fa6c878c7b20d38d8d20b3ca98407e0b0da1
Nirupam <deynirupam12@gmail.com>
2024-01-02 23:51:03 +0530

    adding turret controls

--- Assets/-----Scripts----------/Strategy/Turret/TurretSleeping.cs
+++ Assets/-----Scripts----------/Strategy/Turret/TurretSleeping.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurretLookAtPlayer : MonoBehaviour
+public class TurretSleeping : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
